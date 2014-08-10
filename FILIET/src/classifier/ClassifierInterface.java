b71b20d1516eb81d6c7b8f6686d7f5120d43d3a8
vilsonluffy <vilson_lu@yahoo.com>
2014-08-11 03:34:14 +0800

    Added skeleton code for Classifier Added Weka library (jar)

---
+++ FILIET/src/classifier/ClassifierInterface.java
@@ -0,0 +1,15 @@
+package classifier;
+
+public interface ClassifierInterface {
+	public String classify(String text);
+}
+
+class kNNClassifier implements ClassifierInterface {
+
+	@Override
+	public String classify(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
