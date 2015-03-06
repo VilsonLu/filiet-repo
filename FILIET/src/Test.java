9e7b8b061c53a7131709cc92bcf53bb5f6f227d5
vilsonluffy <vilson_lu@yahoo,com>
2015-03-07 03:30:46 +0800

    Added comments and minor fix
    Moved the adding of the ontology inside the for loop

--- FILIET/src/Test.java
+++ FILIET/src/Test.java
@@ -20,15 +20,17 @@ public class Test {
 		String testTweets = "./resources/tweets/Testing/Test.csv";
 		String ontologyPath = "./resources/ontology/FILIET_Ontology3.owl";
 
-		System.out
-				.println("FILIET - Filipino Information Extraction for Twitter");
+		System.out.println("FILIET - Filipino Information Extraction for Twitter");
 		System.out.println("Running: " + testTweets);
 		System.out.println();
 
+		// modules
 		InformationExtractionEngine extractorEngine = new InformationExtractionEngine();
 		OntologyModule ontology = new OntologyModule();
 		ontology.loadOntology(ontologyPath);
 		
+		
+		// load the dataset
 		List<Sentence> sentences = null;
 		try {
 			sentences = Testing.readTestData(testTweets);
@@ -38,16 +40,12 @@ public class Test {
 		}
 
 		List<Sentence> processedSentences = new ArrayList<Sentence>();
-
-		// String text =
-		// "RT @DZMMTeleRadyo: #walangpasok | Klase sa lahat ng antas sa Mandaluyong City, suspendido bukas (Dec. 8) dahil kay #RubyPH - Mayor Abalos";
+		// run the information extraction engine
 		for (Sentence sentence : sentences) {
 			System.out.println(sentence.getTweets().getTweetID());
 			processedSentences.add(extractorEngine.runExtractor(sentence));
-
-		}
-
-		for (Sentence sentence : processedSentences) {
+			
+			// store the instance to ontology
 			if (sentence.getTweets().getCategory().equalsIgnoreCase("CA")) {
 				CautionAndAdviceTweet ca = Binder.bindCA(sentence);
 				System.out.println("Advice: " + ca.getTweetAdvice());
@@ -58,6 +56,7 @@ public class Test {
 				System.out.println("Date " + ca.getTweetDate());
 				System.out.println("Timestamp " + ca.getTweetTimestamp());
 				System.out.println("Geolocation " + ca.getTweetGeoLocation());
+				ontology.addCautionAndAdviceReport(ca);
 			} else if(sentence.getTweets().getCategory().equalsIgnoreCase("CD")){
 				CasualtiesAndDamageTweet cd = Binder.bindCD(sentence);
 				System.out.println("Detail " + cd.getObjectDetails() );
@@ -71,6 +70,10 @@ public class Test {
 				System.out.println("Geolocation " + cd.getTweetGeoLocation());
 				ontology.addCasualtiesAndDamageReport(cd);
 			}
+
+		}
+		
+
 			
 			ontology.displayStoredTweets();
 			System.out.println();
