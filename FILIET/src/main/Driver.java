77759e645ec275f69ce6358734d1526325227e00
addicteduser <tintin.kalaw@gmail.com>
2015-04-06 13:43:35 +0800

    from jp & kyle

--- FILIET/src/main/Driver.java
+++ FILIET/src/main/Driver.java
@@ -1,5 +1,8 @@
 package main;
 
+import ui.finalUI;
+import java.awt.Dimension;
+
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
@@ -11,15 +14,16 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame frame = new JFrame("FILIET");
+		JFrame frame = new JFrame("FILIET: An Information Extraction System for Filipino Disaster-Related Tweets");
 		frame.add(new finalUI());
 		finalUI ui = new finalUI();
-		frame.setSize(770,640);
+		frame.setSize(885,535);
         Toolkit t = Toolkit.getDefaultToolkit();
         int x = (int)((t.getScreenSize().getWidth() - frame.getWidth()) / 2);
         int y = (int)((t.getScreenSize().getHeight() - frame.getHeight()) / 2);
         frame.setLocation(x, y);
 		frame.setVisible(true);
+		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
